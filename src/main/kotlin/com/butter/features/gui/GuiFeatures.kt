31cfef762b6660d76982b2188287f8c7c58687e5
butterfriez <radradio234@gmail.com>
2022-12-04 20:41:20 -0800

    Changed way of claiming bz orders and confirming ah

--- src/main/kotlin/com/butter/features/gui/GuiFeatures.kt
+++ src/main/kotlin/com/butter/features/gui/GuiFeatures.kt
@@ -3,9 +3,11 @@ package com.butter.features.gui
 import ButterExtras
 import com.butter.config.Config
 import com.butter.events.GuiContainerEvent
+import com.butter.util.Extensions.chest
 import com.butter.util.Extensions.lore
 import com.butter.util.MainUtil.slotClick
 import gg.essential.universal.UChat
+import net.minecraft.client.gui.Gui
 import net.minecraft.client.gui.inventory.GuiChest
 import net.minecraft.inventory.ContainerChest
 import net.minecraftforge.client.event.GuiOpenEvent
@@ -16,48 +18,54 @@ import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent
 object GuiFeatures {
     private var lastClickTime:Long = 0
     private var autoBz = false
+
     @SubscribeEvent
     fun onDrawSlot(e: GuiContainerEvent.DrawSlotEvent) {
         if(e.gui !is GuiChest) return
         val chest = e.gui.inventorySlots
         if (chest is ContainerChest) {
             val name = chest.lowerChestInventory.displayName.formattedText
-
-            //auto claim bazaar orders
-            if (name.contains("Bazaar ➜")
-                && System.currentTimeMillis() - lastClickTime > Config.AutoBazaarClaimDelay
-                && autoBz) {
-                slotClick(chest.windowId, 50)
-                lastClickTime = System.currentTimeMillis()
+            //auto bazaar orders | Main Gui Of Bazaar
+            if(name.contains("Bazaar ➜") && Config.AutoBazaarClaimOrder && e.slot?.stack?.displayName?.contains("Manage Orders") == true && Config.AutoBazaarClaimClaim == 0 && autoBz) {
+                if(System.currentTimeMillis() - lastClickTime > Config.AutoBazaarClaimDelay) {
+                    slotClick(chest.windowId, e.slot.slotNumber)
+                }
             }
-            if(name.contains("Your Bazaar Orders")
-                && autoBz) {
-                chest.inventorySlots.forEach { slot ->
-                    if(slot?.stack?.lore?.contains("§eClick to claim!") == true) {
-                        if(System.currentTimeMillis() - lastClickTime > Config.AutoBazaarClaimDelay) {
-                            slotClick(chest.windowId, slot.slotNumber)
-                            lastClickTime = System.currentTimeMillis()
-                        }
-                    }
+
+            //Bazaar Orders
+            if(name.contains("Bazaar Orders") && Config.AutoBazaarClaimOrder && e.slot?.stack?.displayName?.contains("Click to claim!") == true && Config.AutoBazaarClaimClaim == 0 && autoBz) {
+                if(System.currentTimeMillis() - lastClickTime > Config.AutoBazaarClaimDelay) {
+                    slotClick(chest.windowId, e.slot.slotNumber)
+                }
+            } else if(
+                name.contains("Bazaar Orders")
+                && Config.AutoBazaarClaimOrder
+                && e.slot?.stack?.displayName?.contains("Click to claim!") == true
+                && e.slot?.stack?.displayName?.contains(ButterExtras.mc.thePlayer.name) == true
+                && autoBz)
+            {
+                if(System.currentTimeMillis() - lastClickTime > Config.AutoBazaarClaimDelay) {
+                    slotClick(chest.windowId, e.slot.slotNumber)
                 }
             }
 
             // auto confirm
             if(name.contains("Confirm Purchase") && Config.AutoConfirmAH) {
                 if(Config.AutoConfirmAHMethod == 0) {
-                    if(System.currentTimeMillis() - lastClickTime > Config.AutoConfirmAHClickDelay) {
-                        slotClick(chest.windowId, 11)
-                        lastClickTime = System.currentTimeMillis()
+                    if(e.slot?.stack?.displayName?.contains("Confirm") == true) {
+                        if(System.currentTimeMillis() - lastClickTime > Config.AutoConfirmAHClickDelay) {
+                            slotClick(chest.windowId, e.slot.slotNumber)
+                            lastClickTime = System.currentTimeMillis()
+                        }
                     }
                 }
             }
             if(name.contains("BIN Auction View") && Config.AutoConfirmAH) {
                 if(Config.AutoConfirmAHMethod == 0) {
-                    chest.inventorySlots.forEach {
-                        slot ->
-                        print(slot?.stack?.displayName)
-                        if(slot?.stack?.displayName == "Buy Item Right Now" || slot?.stack?.displayName?.contains("Buy Item Right Now") == true) {
-                            slotClick(chest.windowId, slot.slotNumber)
+                    if(e.slot?.stack?.displayName?.contains("Buy Item Right Now") == true) {
+                        if(System.currentTimeMillis() - lastClickTime > Config.AutoConfirmAHClickDelay) {
+                            slotClick(chest.windowId, e.slot.slotNumber)
+                            lastClickTime = System.currentTimeMillis()
                         }
                     }
                 }
@@ -81,3 +89,4 @@ object GuiFeatures {
         }
     }
 }
+
